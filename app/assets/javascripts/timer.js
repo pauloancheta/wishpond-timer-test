36bf703392ee7a241dde0357f0cfdb5cd3cbbf28
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-06-04 17:36:49 -0700

    fixed my table and put the time-seconds in an array

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
-  var all_dates = $('td.due-date').length
-
+  var all_dates = $('tr').length - 1
   var dateNow = Date.now()/1000;
-
   var dateArray = []
 
   for(var i = 1; i <= all_dates; i++){
-    var date = $('td#todo_' + i + '.due-date').text().split('-');
+    var date = $('td#due-date-todo_' + i).text().split('-');
     date = new Date(parseInt(date[0]), parseInt(date[1]), parseInt(date[2]));
-    dateArray.push( date.getTime()/1000 )
+    dateArray[i] = date.getTime()/1000;
   }
 
-  
+  for(var i = 1;  i <= all_dates; i++){
+    if(dateArray[i] !== 'undefined'){
+      console.log('hello')
+    }
+  }
 
-
+  console.log(dateArray)
   
 });
